c22c24a9051319a1628ab483d2e38ebe88f950e0
rejaevo <rejaevo@gmail.com>
2021-05-23 22:38:15 +0300

    Updates

---
+++ precache-manifest.49d1bdf32e60bf618dbd87c4159ea6df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b4ce225a2796934f7ad57884d9efc7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "292042f73a5b2102f9af",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a4b54382daa41fbcc6ea",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "292042f73a5b2102f9af",
+    "url": "./static/js/2.7341c2fe.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.7341c2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b54382daa41fbcc6ea",
+    "url": "./static/js/main.a418c431.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
